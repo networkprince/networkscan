7c715f54cd0a2e5d7c267c106c4060aee43ec5ec
networkprince <clauber.telles16@gmail.com>
2023-12-15 07:48:14 -0500

    Network Scan

---
+++ portscan.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#Script de socket
+
+import socket,sys
+from alive_progress import alive_bar
+
+print("\nScript de Port Scan")
+ip = input("Digite o IP para realizar o scan de portas: ")
+portas = range(1,65535)
+
+with alive_bar(len(portas), title='Consultando portas') as bar:
+	for porta in portas:
+		meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		meusocket.settimeout(0.5)
+
+		if meusocket.connect_ex((ip,porta)) == 0:
+			print("Porta %i"%porta,"aberta")
+			meusocket.close()
+		bar()
